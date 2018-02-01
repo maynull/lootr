97c4bc49cf909c243fa34f10f8a66dcaab3fc9d5
Burkay Ozdemir <hi@burkayozdemir.com>
2018-02-01 19:14:26 +0300

    droptable updates

--- droptable.js
+++ droptable.js
@@ -1,90 +1,124 @@
 const DropTableObject = function(properties) {
-  this.name = properties.name;
-  this.path = properties.path;
   this.forcedDepth = properties.forcedDepth;
   this.stack = properties.stack;
-  this.weight = properties.weight ? properties.weight : 1;
+  this.weight = properties.weight ? properties.weight : 0;
   this.isLootable = properties.isLootable;
   this.isAlways = properties.isAlways;
-  this.totalWeight = 0;
 };
 DropTableObject.prototype.constructor = DropTableObject;
 
-const DropTable = function(name, object) {
+const DropTable = function(name, obj) {
   this.name = name;
-  this.objects = [];
+  this._obj = obj;
+  this.brancNames = [];
   this.branchs = {};
+  this.totalWeight = 0;
 };
 DropTable.prototype.constructor = DropTable;
+
+DropTable.prototype.randomInRange = function(range) {
+  var bounds = range.split("-");
+  if (bounds.length !== 2) {
+    throw new Error("Wrong format. Correct Example: 2-9");
+  } else {
+    bounds = [bounds[0], bounds[bounds.length - 1]];
+  }
+
+  bounds[0] = parseInt(bounds[0], 10);
+  bounds[1] = parseInt(bounds[1], 10);
+
+  return Math.floor(Math.random() * (bounds[1] - bounds[0])) + bounds[0];
+};
 
 DropTable.prototype.clean = function(path) {
   return path.replace(/^\//g, "").replace(/\/$/g, "");
 };
 
+DropTable.prototype.branch = function(name) {
+  return this.getBranch(name, true);
+};
+
 DropTable.prototype.getBranch = function(name, create) {
   var path = this.clean(name).split("/");
-
   if (!this.branchs[path[0]] && path[0] != this.name && create) {
-    this.branchNames.push(path[0]);
-    this.branchs[path[0]] = new DropTable({ name: path[0] });
+    this.add(new DropTable(path[0]));
   }
   if (path.length === 1) {
     return path[0] === this.name ? this : this.branchs[path[0]];
   } else if (path.length > 1) {
     var head = path.shift();
     var newPath = path.join("/");
-
     if (this.branchs[head]) {
       return this.branchs[head].getBranch(newPath, create);
     }
     if (create) {
-      this.branchNames.push(head);
-      this.branchs[head] = new DropTable({ name: head });
+      this.add(new DropTable(head));
       return this.branchs[head].getBranch(newPath, create);
     }
   }
 };
 
+DropTable.prototype.setWeight = function(value) {
+  var oldWeight = this._obj.weight;
+  this._obj.weight = value;
+  this.totalWeight += value - oldWeight;
+};
+
+DropTable.prototype.totalWeightInItems = function(items) {
+  var totalWeight = 0;
+  items.forEach(element => {
+    totalWeight += items._obj.weight;
+  });
+};
+
 DropTable.prototype.addRange = function(objects) {
   for (var i = 0; i < objects.length; i++) {
-    this.objects.push(new DropTableObject(objects[i]));
+    this.branchs[objects[i].name] = objects[i];
+    this.brancNames.push(objects[i].name);
     this.totalWeight += objects[i].weight;
   }
 };
 
 DropTable.prototype.add = function(object) {
-  this.objects.push(new DropTableObject(object));
+  this.branchs[object.name] = object;
+  this.brancNames.push(object.name);
   this.totalWeight += object.weight;
 };
 
-DropTable.prototype.roll = function() {
-  var nextBranch;
-  /*do {
-      var item = randomPick(this.objects);
+DropTable.prototype.allItemsInDepth = function(depth) {
+  var items = [];
+
+  for (var i = 0; i < this.branchNames.length; i++) {
+    items.push(this.branchs[this.branchNames[i]]);
+    if (depth > 0) {
+      items = items.concat(
+        this.branchs[this.branchNames[i]].allItemsInDepth(depth - 1)
+      );
     }
-    while( item.)*/
+  }
+  return items;
 };
 
-DropTable.prototype.randomPick = function(items) {
-  var targetWeight = ~~(Math.random() * this.getTotalWeight(items));
+DropTable.prototype.randomPick = function() {
+  var targetWeight = ~~(Math.random() * this._obj.totalWeight);
   var currentWeight = 0;
   var pickedItem = null;
-  for (var i = 0; i < items.length; i++) {
-    currentWeight += items[i].weight;
-    if (currentWeight < targetWeight) pickedItem = items[i];
+  for (var i = 0; i < this.branchNames.length; i++) {
+    currentWeight += this.branchs[this.branchNames[i]].weight;
+    if (currentWeight > targetWeight)
+      pickedItem = this.branchs[this.branchNames[i - 1]];
   }
 
   return pickedItem;
 };
 
-Lootr.prototype.roll = function(catalogPath, allowedNesting, threshold) {
-  var branch = this.getBranch(catalogPath, false);
-  var items = this.allItems(allowedNesting);
-  return branch.randomPick(
-    items,
-    allowedNesting,
-    threshold === undefined ? 1.0 : threshold
-  );
+DropTable.prototype.roll = function(catalogPath, forcedDepth) {
+  var nextBranch = this.getBranch(catalogPath, false);
+  var item = nextBranch.randomPick();
+  if (!item.isLootable || forcedDepth > 0) {
+    item = this.roll(item, forcedDepth - 1);
+  }
+  return item;
 };
 
 const LootManager = function(lootr) {
@@ -97,7 +131,7 @@ LootManager.prototype.addDropTable = function(dropTable) {
   this.dropTables.push(dropTable);
 };
 
-LootManager.prototype.loot = function(dropTableName) {
+LootManager.prototype.loot = function(dropTableName, range) {
   if (this.dropTables.length === 0 || this.dropTables === undefined)
     return null;
 
@@ -109,9 +143,6 @@ LootManager.prototype.loot = function(dropTableName) {
   for (var i = 0; i < dropTable.objects.length; i++) {
     var doObject = dropTable.objects[i];
   }
-
-  //TODO:buraya bakılır
-  rewards.push(this.getAlwaysPicks());
 
   for (var i = 0; i < this.dropTable.length; i++) {
     var item = this.roll(
@@ -153,72 +184,3 @@ LootManager.prototype.loot = function(dropTableName) {
 
   return rewards;
 };
-
-function Lootr(name, weight, isAlways) {
-  name = name || "root";
-  name = this.clean(name);
-  weight = weight ? 1 : weight;
-  isAlways = isAlways === undefined ? false : isAlways;
-
-  if (name.indexOf("/") > -1) {
-    throw new Error("specified name should not contain a / separator");
-  }
-
-  this.rootItem = new Item({ name, weight, isAlways });
-  this.items = [];
-  this.branchs = {};
-  this.modifiers = [];
-}
-Lootr.prototype.allItems = function(nesting) {
-  var items = this.items.slice();
-
-  nesting = nesting === undefined ? this.branchs.length : nesting;
-  if (nesting > 0) items = items.concat(this.branchs[i].allItems(nesting - 1));
-
-  return items;
-};
-Lootr.prototype.getTotalWeight = function(items) {
-  var totalWeight = 0;
-  for (var i = 0; i < items.length; i++) {
-    var item = items[i];
-    if (!items.isAlways) totalWeight += item.weight;
-  }
-  return totalWeight;
-};
-Lootr.prototype.add = function(item, path) {
-  const _item = new Item(item);
-  if (path === undefined) {
-    this.items.push(_item);
-  } else {
-    var branch = this.branch(path);
-    branch.items.push(_item);
-  }
-
-  return this;
-};
-Lootr.prototype.randomPick = function(items, threshold) {
-  var weightOfPickedItem = ~~(Math.random() * this.getTotalWeight(items));
-  var currentWeight = 0;
-  var pickedItem = null;
-  for (var i = 0; i < items.length; i++) {
-    currentWeight += items[i].weight;
-    if (currentWeight < weightOfPickedItem) pickedItem = items[i];
-  }
-
-  return pickedItem;
-};
-
-Lootr.prototype.getAlwaysPicks = function(items) {
-  var alwaysItems = [];
-  for (var i = 0; i < items.length; i++) {
-    if (item.isAlways) alwaysItems.push(alwaysItems);
-  }
-  return alwaysItems;
-};
-
-const Item = function(properties) {
-  this.name = properties.name;
-  this.isAlways = properties.isAlways;
-  this.weight = properties.weight;
-};
-Item.prototype.constructor = Item;
